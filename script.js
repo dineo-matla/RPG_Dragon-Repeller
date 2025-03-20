490e086e82f377b9673496008183f135b65d424e
dineo-matla <dineomatla@outlook.com>
2025-03-20 19:59:20 +0200

    Refactored code to improve organization and efficiency

--- script.js
+++ script.js
@@ -18,7 +18,11 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
-function goTown() {
+function goTown() {}
+
+function goStore() {}
+
+function update(location) {
   button1.innerText = "Go to store";
   button2.innerText = "Go to cave";
   button3.innerText = "Fight dragon";
@@ -28,20 +32,6 @@ function goTown() {
   text.innerText =
     'You are in the town square. You see a sign that says "Store".';
 }
-
-function goStore() {
-  button1.innerText = "Buy 10 health (10 gold)";
-  button2.innerText = "Buy weapon (30 gold)";
-  button3.innerText = "Go to town square";
-
-  button1.onclick = buyHealth;
-  button2.onclick = buyWeapon;
-  button3.onclick = goTown;
-
-  text.innerText = "You enter the store.";
-}
-
-function update(location) {}
 
 const locations = [
   {
